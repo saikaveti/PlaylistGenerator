f2ef0b9beac8e0d1db54679e51208738e66ab239
saikaveti <saileshkaveti@hotmail.com>
2018-02-16 21:07:40 -0500

    tryguy

--- Test.py
+++ Test.py
@@ -1,6 +1,10 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
+
 client_credentials_manager = SpotifyClientCredentials(client_id='264a37295829463e9c1dceaa9562fa83', client_secret='ce4945a01e554f9390459744f92a12b1')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
+results = sp.search(q='weezer', limit=20)
+for i, t in enumerate(results['tracks']['items']):
+    print (' ', i, t['name'])
