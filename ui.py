5fbdb539c22f1eabc93b434fd7ae4deb4ea9287b
Bepen Neupane <bepen3@gmail.com>
2018-02-17 11:17:44 -0500

    fuzzy elephant

--- ui.py
+++ ui.py
@@ -1,5 +1,9 @@
+from tkinter import *
 
-import tkinter
-window = tkinter.Tk()
-window.title("Spotiplay")
+window = Tk()
+window.title("Fuzzy Elephant")
+window.configure(background="black")
+logo = PhotoImage(file="logo.gif")
+Label (window, image=logo, bg="black") .grid(row=0, column=0, sticky=E)
+#id = window.create_rectangle(100, 100, 100, 100, fill="black")
 window.mainloop()
